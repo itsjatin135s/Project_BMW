7d6b2191b426eb2c72ae680f3c4422e8a89f19cd
jatinsaini137 <jatinrajbagri2001@gmail.com>
2021-03-11 23:20:10 +0530

    adding python files and database file inital stage

---
+++ database_handler.py
@@ -0,0 +1,36 @@
+import sqlite3
+
+def createtable():
+	conn = sqlite3.connect('Admin.db')
+	cur = conn.cursor()
+	cur.execute("CREATE TABLE user(id INTEGER PRIMARY KEY,uname TEXT NOT NULL,password UNIQUE NOT NULL )")
+	conn.commit()
+	conn.close()
+	return "table created"
+
+def insert():
+	conn = sqlite3.connect("Admin.db")
+	cur = conn.cursor()
+	cur.execute("INSERT INTO user VALUES(11,'rojee2','password2')")
+	conn.commit()
+	conn.close()
+	return "success"
+
+def fetch():
+	conn = sqlite3.connect("Admin.db")
+	cur = conn.cursor()
+	cur.execute("SELECT password FROM user WHERE uname='rojee4'")
+	data = cur.fetchall()
+	conn.commit()
+	conn.close()
+	if len(data)==0:
+		print ("kachu bhi na ya  me")
+	else:
+		print("kuch to gadbad hai daya")
+	return data
+
+if __name__ == '__main__':
+	print(fetch())
+
+
+	  
